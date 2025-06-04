5be4655023ff95a8ac5aab0c780074e0b88c98d5
corinamihaila30 <119610150+corinamihaila30@users.noreply.github.com>
2025-06-04 17:09:50 +0300

    Delete fisiereApp directory

--- fisiereApp/shared/protocol.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def create_message(action, path, content=None):
-    return json.dumps({"action": action, "path": path, "content": content})
-
-def parse_message(message):
-    return json.loads(message)
